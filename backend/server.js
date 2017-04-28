51d1a9661383ff8ca1414eb06b6f07af2f3de04b
juliemdyer <juliemdyer@gmail.com>
2017-04-28 11:33:37 -0400

    Added edit mode

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.get('/api/contacts', (req, resp, next) => {
         .catch(next);
 })
 
+// Add new contact to database and return all the info about that contact, send over to front end so front end can update the state.
 app.post('/api/contacts', (req, resp, next) => {
     let data = req.body;
     db.one(`insert into contact values (default, $1, $2, $3, $4, $5) returning *`, [data.name, data.phone, data.email, data.type, data.favorite])
@@ -22,9 +23,17 @@ app.post('/api/contacts', (req, resp, next) => {
         .catch(next);
 })
 
-app.post('/api/delete', (req, resp, next) => {
-    let id = req.body.id;
+app.delete('/api/contact/:id', (req, resp, next) => {
+    let id = req.params.id;
     db.one(`delete from contact where id = $1 returning id`, id)
+        .then(result => resp.json(result))
+        .catch(next);
+})
+
+app.put('/api/contact/:id', (req, resp, next) => {
+    let id = req.params.id;
+    let data = req.body;
+    db.one(`update contact set name = $1, phone = $2, email = $3, type=$4, favorite = $5 where id = $6 returning *`, [data.name, data.phone, data.email, data.type, data.favorite, id])
         .then(result => resp.json(result))
         .catch(next);
 })
